e07777f4b9818781b97531d801f017f97e6eeb4f
Brayan Valdebenito <brayan.valdebenito@gmail.com>
2022-11-23 13:12:08 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import NavBar from './components/NavBar'
+import CartWidget from './components/CartWidget'
+import ItemListContainer from './components/ItemListContainer'
+
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <NavBar/>
+        <CartWidget/>
+      <ItemListContainer greeting='hola! '/>
+{/* en este orden deberia ir, no es un argumento el cart widget, es un elemento que va despues del navbar y se acopla */}
+
+      </div>
+    </>
+    );
+  }
+  
+  export default App;
+  
